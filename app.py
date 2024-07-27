fdebb55709784d4e69a9316c3c5b21b1f7d52d3e
Julia Chen <juch5453@colorado.edu>
2024-07-27 10:45:00 -0700

    connect to new db

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import psycopg2
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello World from Julia Chen in 3308'
+
+@app.route('/db_test')
+def db_test():
+    #conn = psycopg2.connect("postgresql://juch5453_postgresql_user:tD7NdcQPZM3jriCWxcklHUPaaID93k9I@dpg-cqij026ehbks73buv140-a.oregon-postgres.render.com/juch5453_postgresql")
+    conn = psycopg2.connect("postgresql://juch5453_postgresql_user:tD7NdcQPZM3jriCWxcklHUPaaID93k9I@dpg-cqij026ehbks73buv140-a/juch5453_postgresql")
+    conn.close()
+    return "Database Connection Successful"
